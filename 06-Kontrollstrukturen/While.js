7fafcd9a80d4ea9c5b3fbb9091c161fcbf9a0966
AnesJusupovic <jusanes04@gmail.com>
2022-02-12 10:52:35 +0100

    While Schleife

---
+++ 06-Kontrollstrukturen/While.js
@@ -0,0 +1,28 @@
+"use strict"
+
+/*
+let counter = 0
+while (counter < 5) {
+    console.log(counter);
+    counter++;
+}
+*/
+
+// Aufpassen! Endlosschleife 
+
+/*
+while (true) {
+    console.log("Hallo Welt!")
+}
+*/
+/*
+let students = ["Max"];
+
+while (students.length < 4) {
+    console.log("Hallo Welt!");
+}
+*/
+
+let counter = 0;
+do { counter++;
+    console.log("Hallo Welt!") } while (counter < 5)
