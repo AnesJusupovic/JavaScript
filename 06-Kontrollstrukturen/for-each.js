69098e98362fcd7f261e5c75b620ef86ef719b93
AnesJusupovic <jusanes04@gmail.com>
2022-02-12 13:47:41 +0100

    For-Each

--- 06-Kontrollstrukturen/for-each.js
+++ 06-Kontrollstrukturen/for-each.js
@@ -1,3 +1,16 @@
 "use strict"
 
 const students = ["Max", "Erika", "Monika"]
+
+for (let i = 0; i < students.length; i++) {
+    console.log("i:", i);
+    console.log(students[i])
+}
+
+for (const i in students) {
+    console.log(students[i]);
+}
+
+for (const student of students) {
+    console.log(student)
+}
