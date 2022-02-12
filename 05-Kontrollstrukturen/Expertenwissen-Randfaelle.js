ea9405f5ddb0c0b3a2f477fe6f8dc1e4c9500a8b
AnesJusupovic <jusanes04@gmail.com>
2022-02-12 10:45:27 +0100

    Beendet Kontrollstrukturen 1

---
+++ 05-Kontrollstrukturen/Expertenwissen-Randfaelle.js
@@ -0,0 +1,20 @@
+"use strict"
+
+// Randfälle beachten!
+
+// Ganze Zahl, größer als 0
+let numberOfStudents = 5;
+
+if (numberOfStudents === 1) {
+    console.log("Der Sprachkurs kostet 40Euro / Stunde / Teilnehmer")
+} else {
+    // numberOfStudents ist eine ganze Zahl, größer als 1
+    // numberOfStudents ist eine ganze Zahl, größer gleich 2
+    if (numberOfStudents <= 5) {
+        console.log("25 EUro / Stunde / Teilnehmer")
+    } else {
+        // numberOfStudents ist eine ganze Zahl, größer als 5 
+        // numberOfStudents ist eine ganze Zahl, größer gleich 6
+        console.log("Der Sprachkurs kostet 15Euro / Stunde / Teilnehmer")
+    }
+}
